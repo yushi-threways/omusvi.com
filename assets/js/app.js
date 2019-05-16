f955a2df24d33b762d7de5f9f03aed186382784f
yushi-threways <seki@threways.co.jp>
2019-05-16 19:03:23 +0900

    bootstrap font-awasomeを導入

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,9 +6,11 @@
  */
 
 // any CSS you require will output into a single css file (app.css in this case)
-require('../css/app.css');
+// require('../css/app.css');
+import '../css/app.scss';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
-// const $ = require('jquery');
+const $ = require('jquery');
+global.$ = global.jQuery = $;
 
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+require('bootstrap');
