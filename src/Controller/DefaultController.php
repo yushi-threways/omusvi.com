551d185b9c900adfd4999af8895f9f3d0cace747
yushi-threways <seki@threways.co.jp>
2019-09-12 18:09:27 +0900

    トップページにイベント表示のメソッドを追加

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,10 +2,12 @@
 declare(strict_types=1);
 namespace App\Controller;
 
+use App\Entity\MyEvent;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\MyEventRepository;
 
 /**
  * Class DefaultController
@@ -20,7 +22,12 @@ class DefaultController extends AbstractController
      */
     public function index(Request $request): Response
     {
+        /** @var MyEventRepository $repo */
+        $repo = $this->getDoctrine()->getRepository(MyEvent::class);
+        $myEvents -> $repo->findAll();
+
         return $this->render('default/index.html.twig', [
+            'myEvents' => $myEvents,
         ]);
     }
 }
