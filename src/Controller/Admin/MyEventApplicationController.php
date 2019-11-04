240398dcfb2c213808bfbc4f5337d7b4b594dfcd
yushi-threways <seki@threways.co.jp>
2019-11-04 20:37:06 +0900

    画像の保存のパスを変更

---
+++ src/Controller/Admin/MyEventApplicationController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\MyEventApplication;
+use App\Entity\MyEventSchedule;
+use App\Entity\UserDetail;
+use App\Form\Type\MyEventApplicationType;
+use App\Form\Type\ConfirmFormType;
+use App\Repository\MyEventApplicationRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/event/applicatoin")
+ */
+class MyEventApplicationController extends AbstractController
+{
+    /**
+     * @Route("/", name="admin_my_event_application", methods={"GET"})
+     */
+   public function index()
+   {
+
+
+   }
+}
