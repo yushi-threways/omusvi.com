829caefb18e1e8b509e65feb79dbf369b69767fb
takabayashi <you@example.com>
2020-01-21 19:12:48 +0900

    イベント会場が未定の時の

--- src/Controller/MyEventVenueController.php
+++ src/Controller/MyEventVenueController.php
@@ -9,6 +9,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Knp\Component\Pager\PaginatorInterface;
+
 
 /**
  * @Route("/venue")
@@ -18,10 +20,25 @@ class MyEventVenueController extends AbstractController
     /**
      * @Route("/", name="my_event_venue_index", methods={"GET"})
      */
-    public function index(MyEventVenueRepository $myEventVenueRepository): Response
+    public function index(Request $request, MyEventVenueRepository $myEventVenueRepository, PaginatorInterface $paginator): Response
     {
+
+        $query = $myEventVenueRepository->findAll();
+
+        $pagination = $paginator->paginate(
+            $query, /* query NOT result */
+            $request->query->getInt('page', 1), /*page number*/
+            10 /*limit per page*/
+        );
+
+
+        $pagination_data = $pagination->getPaginationData();
+
         return $this->render('my_event_venue/index.html.twig', [
-            'my_event_venues' => $myEventVenueRepository->findAll(),
+            'pagination' => $pagination,
+            'total' => $pagination->getTotalItemCount(),
+            'start' => $pagination_data['firstItemNumber'],
+            'end' => $pagination_data['lastItemNumber']
         ]);
     }
 }
